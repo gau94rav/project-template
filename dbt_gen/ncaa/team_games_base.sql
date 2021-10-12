9d9c2c989edb0aae3fdfce27465f86e593af05c2
kanav@hashpath.com <kanav@hashpath.com>
2021-10-12 14:11:11 +0000

    Test

--- dbt_gen/ncaa/team_games_base.sql
+++ dbt_gen/ncaa/team_games_base.sql
@@ -3,10 +3,6 @@ SELECT *
 FROM `topcoat-internal.dbt_kmasters.teams_games`
 
 where team_name = '49ers'
-<<<<<<< HEAD
-and season = '2015'
-=======
 and season = '2015'
 
 as
->>>>>>> 24c4ca4f2858539ae0a49d32bea62b7cd3c75749
